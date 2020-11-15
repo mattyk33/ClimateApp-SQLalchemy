8ad2a53a28839c18830ecf07f3b3a5f065caeeea
Matthew Kukulski <Matthew.Kukulski@du.edu>
2020-11-14 21:01:19 -0700

    precipitation route

--- climate.py
+++ climate.py
@@ -41,5 +41,27 @@ def welcome():
         f"/api/v1.0/&lt;start&gt;<br/>"
         f"/api/v1.0/&lt;start&gt;/&lt;end&gt;")
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Calculate the date 1 year ago from the last data point in the database
+    one_year_ago = dt.date(2017,8,23)-dt.timedelta(days=365)
+
+    # Query for the dates and precipitation values
+    precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>=one_year_ago).all()
+
+    # Create dictionary
+    precip_dict = {}
+
+    # Loop to add keys and values to dictionary
+    for date, prcp in precip:
+        precip_dict[date] = prcp
+
+    session.close()
+
+    return jsonify(precip_dict)
+
 if __name__ == '__main__':
     app.run(debug=True)
